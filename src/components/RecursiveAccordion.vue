997b4af283a7a0e969f25b51d5948d37f8001ad6
Robert Al-Romhein <robert.alromhein@gmail.com>
2017-12-31 16:59:00 -0500

    Add depth

--- src/components/RecursiveAccordion.vue
+++ src/components/RecursiveAccordion.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="recursive-accordion">
+  <div class="recursive-accordion" :style="{paddingLeft: depth * 16 + 'px'}">
     <h1>{{ node.title }}</h1>
-    <RecursiveAccordion v-for="(child, childIndex) in node.children" :key="childIndex" :node="child" />
+    <RecursiveAccordion
+      v-for="(child, childIndex) in node.children" :key="childIndex"
+      :node="child"
+      :depth="depth + 1"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: 'RecursiveAccordion',
-  props: ['node']
+  props: {
+    node: {
+      type: Object
+    },
+    depth: {
+      type: Number,
+      default: 0
+    }
+  }
 }
 </script>
 

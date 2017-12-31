65f7a74520b6598a2f6521572daad455197619bb
Robert Al-Romhein <robert.alromhein@gmail.com>
2017-12-31 15:45:09 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <RecursiveAccordion :node="categories" />
+  </div>
+</template>
+
+<script>
+import RecursiveAccordion from './components/RecursiveAccordion'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      categories: {
+        title: 'Root Node',
+        children: [
+          {
+            title: 'Child Node #1',
+            children: [
+              {
+                title: 'Deep Child Node'
+              }
+            ]
+          },
+          {
+            title: 'Child Node #2'
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    RecursiveAccordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/RecursiveAccordion.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="recursive-accordion">
+    <h1>{{ node.title }}</h1>
+    <RecursiveAccordion v-for="(child, childIndex) in node.children" :key="childIndex" :node="child" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecursiveAccordion',
+  props: ['node']
+}
+</script>
+
+<style>
+</style>
